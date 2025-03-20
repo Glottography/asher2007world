188ba220475172b221091d292cb552fdfa9851a5
Robert Forkel <xrotwang@googlemail.com>
2025-03-20 09:22:10 +0100

    fixed problems with glottolog distance for traditional dataset

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# asher2007world
-Language areas from Asher &amp; Moseley 2007 "Atlas of the World's Languages"
+# Glottography dataset derived from Asher and Moseley 2007 "Atlas of the World's Languages"
+
+[![CLDF validation](https://github.com/Glottography/asher2007world/workflows/CLDF-validation/badge.svg)](https://github.com/Glottography/asher2007world/actions?query=workflow%3ACLDF-validation)
+
+## How to cite
+
+If you use these data please cite
+- the original source
+  > Asher, R. E. & Christopher J. Moseley (eds.) 2007. Atlas of the World's Languages. 2nd edn. Routledge.
+- the derived dataset using the DOI of the [particular released version](../../releases/) you were using
+
+## Description
+
+
+This dataset is licensed under a CC-BY-4.0 license
+
+
+
+
+### Coverage
+
+```geojson
+{"type": "Feature", "geometry": {"type": "Polygon", "coordinates": [[[-171.9, -56.0], [-171.9, 80.6], [180.0, 80.6], [180.0, -56.0], [-171.9, -56.0]]]}, "properties": {}}
+```
+
+
+## CLDF Datasets
+
+The following CLDF datasets are available in [cldf](cldf):
+
+- CLDF [Generic](https://github.com/cldf/cldf/tree/master/modules/Generic) at [cldf/traditional/traditional-metadata.json](cldf/traditional/traditional-metadata.json)
+- CLDF [Generic](https://github.com/cldf/cldf/tree/master/modules/Generic) at [cldf/contemporary/contemporary-metadata.json](cldf/contemporary/contemporary-metadata.json)
--- cldfbench_asher2007world.py
+++ cldfbench_asher2007world.py
@@ -1,187 +1,233 @@
 import json
-import shutil
 import pathlib
-import functools
-import subprocess
+import itertools
 import collections
 
-from cldfbench import Dataset as BaseDataset
-from cldfgeojson import aggregate
-from pycountry import languages
-from shapely.geometry import shape, Polygon, MultiPolygon, GeometryCollection, LineString, MultiLineString, Point
-from shapely import distance, to_geojson, simplify
-from shapely.validation import explain_validity, make_valid
+import cldfbench
+from shapely import intersection_all
+from cldfgeojson import MEDIA_TYPE, aggregate, feature_collection
+from cldfgeojson.geojson import dump
 from clldutils.jsonlib import dump
-from csvw.dsv import UnicodeWriter
-from cldfgeojson.create import merged_geometry
+from clldutils.markup import add_markdown_text
+from pycldf.sources import Sources
+
+import pyglottography
+from pyglottography.dataset import Feature
 
 
-class Dataset(BaseDataset):
+class Dataset(pyglottography.Dataset):
     dir = pathlib.Path(__file__).parent
     id = "asher2007world"
 
-    def cldf_specs(self):  # A dataset must declare all CLDF sets it creates.
-        return super().cldf_specs()
-
-    def cmd_download(self, args):
-        sdir = self.dir.parent / 'glottography-data' / 'worldatlas'
-        shutil.copy(sdir / 'source' / '{}.bib'.format(sdir.name), self.raw_dir / 'sources.bib')
-        # We want a valid geopackage:
-        subprocess.check_call([
-            'ogr2ogr',
-            str(self.raw_dir / 'dataset.gpkg'),
-            str(sdir / '{}.gpkg'.format(sdir.name)),
-            '-makevalid',
-            '-nlt', 'PROMOTE_TO_MULTI',
-        ])
-        subprocess.check_call([
-            'ogr2ogr',
-            str(self.raw_dir / 'dataset.geojson'),
-            str(self.raw_dir / 'dataset.gpkg'),
-            '-t_srs', 'EPSG:4326',
-            '-s_srs', 'EPSG:3857',
-        ])
-
-    @functools.cached_property
-    def poly2gc(self):
-        res = collections.defaultdict(list)
-        for row in self.etc_dir.read_csv('polygons.csv', dicts=True):
-            res[row['name']].append((
-                Point(float(row['lon']), float(row['lat'])),
-                row['glottocode']))
-        return res
-
-    def iter_polys(self):
-        i = 0
-        invalid, polys, shapes = 0, [], []
-        old, new, removed, weird = [], [], [], []
-        maps = set()
-        for f in self.raw_dir.read_json('dataset.geojson')['features']:
-            if 'Contemporary' in f['properties']['map_name_full']:
-                continue  # FIXME: Handle those, too!
-            maps.add(f['properties']['map_name_full'])
-            obj = shape(f['geometry'])
-            if not obj.is_valid:
-                invalid += 1
-                old.append(f)
-                obj = make_valid(obj)
-                if isinstance(obj, GeometryCollection):
-                    if (isinstance(obj.geoms[0], (MultiPolygon, Polygon))
-                            and isinstance(obj.geoms[1], (LineString, MultiLineString))):
-                        # Something sticking out. We drop the LineString.
-                        removed.append(dict(type='Feature', geometry=json.loads(to_geojson(obj.geoms[1]))))
-                        obj = obj.geoms[0]
-                    else:
-                        raise ValueError
-
-                if not obj.is_valid:
-                    raise ValueError
-                    print(explain_validity(obj))
-
-                new.append(dict(type='Feature', geometry=json.loads(to_geojson(obj))))
-
-            if isinstance(obj, Polygon):
-                obj = MultiPolygon([obj])
-
-            #shapes.append(json.loads(to_geojson(simplify(obj, 0.5))))
-            assert isinstance(obj, MultiPolygon), obj
-            assert obj.is_valid
-
-            for poly in obj.geoms:
-                #
-                # FIXME: possibly update metadata!
-                #
-                cent = poly.representative_point()
-                if not poly.contains(cent):
-                    # ignore these mini polygons.
-                    assert poly.area < 1.1473590666151532e-17
-                else:
-                    polys.append([f['properties']['name'], f['properties']['glottocode'], cent.x, cent.y])
-
-                for point, code in self.poly2gc.get(f['properties']['name'], []):
-                    if poly.contains(point):
-                        gc = code
-                        break
-                else:
-                    gc = f['properties']['glottocode']
-                if gc:
-                    i += 1
-                    yield (
-                        i,
-                        dict(
-                            type='Feature',
-                            properties=f['properties'],
-                            geometry=json.loads(to_geojson(poly))),
-                        gc)
-
-                #except AssertionError:
-                #    print(distance(poly, cent))
-        #dump(dict(features=removed, type='FeatureCollection'), self.dir / 'removed.geojson')
-        #dump(dict(features=old, type='FeatureCollection'), self.dir / 'tofix.geojson')
-        #dump(dict(features=new, type='FeatureCollection'), self.dir / 'fixed.geojson')
-        #dump(dict(features=[dict(type='Feature', geometry=merged_geometry(shapes))], type='FeatureCollection'), self.dir / 'all.geojson')
-        #with UnicodeWriter(self.etc_dir / 'polygons.csv') as csvfile:
-        #    csvfile.writerow(['name', 'glottocode', 'lon', 'lat'])
-        #    csvfile.writerows(polys)
-
-        #for n in sorted(maps, key=lambda x: int(x.split()[1])):
-        #    print(n)
+    def cldf_specs(self):
+        return {
+            'traditional': cldfbench.CLDFSpec(
+                metadata_fname='traditional-metadata.json',
+                dir=self.cldf_dir / 'traditional',
+                module='Generic',
+                writer_cls=cldfbench.CLDFWriter,
+            ),
+            'contemporary': cldfbench.CLDFSpec(
+                metadata_fname='contemporary-metadata.json',
+                dir=self.cldf_dir / 'contemporary',
+                module='Generic',
+                writer_cls=cldfbench.CLDFWriter,
+            ),
+        }
 
     def cmd_makecldf(self, args):
-        """
-        Convert the raw data to a CLDF dataset.
+        gc_by_extension = collections.defaultdict(set)
+        book, unattested, unclassifiable = set(), set(), set()
+        for lg in args.glottolog.api.languoids():
+            gc_by_extension[lg.id].add(lg.id)
+            for _, gc, _ in lg.lineage:
+                gc_by_extension[gc].add(lg.id)
+                if gc == 'unat1236':
+                    unattested.add(lg.id)
+                if gc == 'book1242':
+                    book.add(lg.id)
+                if gc == 'uncl1493':
+                    unclassifiable.add(lg.id)
+        gc_by_extension = sorted(gc_by_extension.items(), key=lambda x: len(x[1]))
 
-        >>> args.writer.objects['LanguageTable'].append(...)
-        """
-        from cldfgeojson import MEDIA_TYPE
-        from cldfgeojson.create import aggregate
+        # How to treat polygons assigned to multiple languages?
+        # Status quo: Polygons are duplicated - thus, to figure out that multiple languages are
+        # assigned to the same polygon, one would need to use shapely, e.g. computing overlap and
+        # math.is_close or areas. It seems more correct, to just have one polygon and reference
+        # that from multiple languages. In aggregations, this polygon would only show up if all
+        # languoids are dialects of the same language or members of the same family.
+        features = []
 
-        args.writer.cldf.add_component('MediaTable')
-        args.writer.cldf.add_component(
-            'LanguageTable',
-            {
-                'name': 'Speaker_Area',
-                'propertyUrl': 'http://cldf.clld.org/v1.0/terms.rdf#speakerArea'
-            })
+        def size(shp):
+            mult = 100000000
+            return int(shp.area * mult)
 
-        features, languages = aggregate(list(self.iter_polys()), args.glottolog.api)
-        #
-        # FIXME: split geojson in per-family files?!
-        #
-        dump(dict(type='FeatureCollection', features=features), self.cldf_dir / 'languages.geojson')
+        areas = collections.defaultdict(list)
+        for pid, f, gc in self.features:  # Group features by area.
+            areas[size(f.shape)].append((pid, f, gc))
+        for a, polys in sorted(areas.items(), key=lambda x: x[0]):
+            if len(polys) > 1:
+                gc = None
+                # Only keep languages matched to non-bookkeeping Glottolog languoids.
+                gcs = set(p[2] for p in polys if p[2] and p[2] not in book and p[2] not in unattested and p[2] not in unclassifiable)
+                if len(gcs) > 1:
+                    for gc, ext in gc_by_extension:
+                        if ext.issuperset(gcs):
+                            break  # All languoids are within one group. Match to this code, keep all names.
+                    else:
+                        pass  # Don't map to a Glottocode, keep all names.
+                    # Reduce to one polygon, matched to the smallest Glottolog group
+                    # containing all matched languoids.
+                else:  # Use just one polygon, matched to the single Glottocode (but keep all names!)
+                    gc = gcs.pop() if gcs else None
 
-        for (glang, pids, family), f in zip(languages, features):
-            if 1:#lids is None or (glang.id not in lids):  # Don't append isolates twice!
-                args.writer.objects['LanguageTable'].append(dict(
-                    ID=glang.id,
-                    Name=glang.name,
-                    Glottocode=glang.id,
-                    Latitude=glang.latitude,
-                    Longitude=glang.longitude,
-                    #Contribution_IDs=pids,
-                    Speaker_Area='languages',
-                    #Glottolog_Languoid_Level=ptype,
-                    #Family=family,
+                assert int(size(intersection_all([p.shape for _, p, _ in polys]))) == a
+                # Yield just one shape, with updated metadata.
+                features.append((
+                    list(sorted([pid for pid, _, _ in polys], key=lambda x: int(x))),
+                    polys[0][1],
+                    gc))
+            else:  # All good, only one language mapped to the polygon.
+                features.append(([polys[0][0]], polys[0][1], polys[0][2]))
+
+        fi = self.feature_inventory
+        for fid, v in fi.items():
+            maps = [s.strip() for s in v.properties['map_name_full'].split('|') if s.strip()]
+            numbers = [s.strip() for s in v.properties['number_legend'].split('|') if s.strip()]
+            assert len(maps) == len(numbers), v.id
+            v.properties['maps'] = ['{} [{}]'.format(m, n) for m, n in zip(maps, numbers)]
+
+        def merge_properties(f, pids):
+            # name, map_name_full, cldf:languageReference - remove number_legend
+            f['properties']['name'] = ' | '.join(fi[pid].name for pid in pids)
+            f['properties']['cldf:languageReference'] = '|'.join(fi[pid].glottocode for pid in pids if fi[pid].glottocode)
+            f['properties']['maps'] = sorted(set(itertools.chain.from_iterable(fi[pid].properties['maps'] for pid in pids)))
+            return f
+
+        def is_(year, period):
+            what = 'time of contact' if period == 'traditional' else period
+            return year == what or year == '2007'
+
+        for period in ['traditional', 'contemporary']:
+            with (self.cldf_writer(args, cldf_spec=period) as writer):
+                self.schema(writer.cldf)
+                writer.cldf.properties['dc:title'] = \
+                    ("{} speaker areas derived from Asher & Moseley 2007 "
+                     "\"Atlas of the World's Languages\"").format(period.capitalize())
+                #
+                # FIXME: Add dc:description!
+                #
+                writer.cldf.add_columns(
+                    'LanguageTable',
+                    {'name': 'Maps', 'separator': '; '},
+                )
+                writer.cldf.add_columns(
+                    'ContributionTable',
+                    {'name': 'Maps', 'separator': '; '},
+                    {'name': 'Equivalent_Feature_IDs', 'separator': ' '},
+                )
+                writer.cldf.add_sources(*Sources.from_file(self.etc_dir / "sources.bib"))
+
+                fs = []
+                for pids, f, gc in features:
+                    years = {fi[pid].year for pid in pids}
+                    assert len(years) == 1
+                    if not is_(years.pop(), period):
+                        continue
+                    fs.append(merge_properties(f, pids))
+                    writer.objects['ContributionTable'].append(dict(
+                        ID=pids[0],
+                        Name=f.properties['name'],
+                        Glottocode=gc or None,
+                        Source=[self.id],
+                        Media_ID='features',
+                        Maps=f.properties['maps'],
+                        Equivalent_Feature_IDs=pids[1:],
+                        Year='traditional' if fi[pids[0]].year == 'time of contact' else '2007',
+                    ))
+                dump(
+                    feature_collection(
+                        fs,
+                        **{
+                            'description': self.metadata.description,
+                            'dc:isPartOf': self.metadata.title,
+                        }),
+                    writer.cldf_spec.dir / 'features.geojson')
+                writer.objects['MediaTable'].append(dict(
+                    ID='features',
+                    Name='{} areas depicted in the source'.format(period),
+                    Media_Type=MEDIA_TYPE,
+                    Download_URL='features.geojson',
                 ))
-        args.writer.objects['MediaTable'].append(dict(
-            ID='languages',
-            Name='Speaker areas for languages',
-            Description='Speaker areas aggregated for Glottolog {}-level languoids, '
-                        'color-coded by family.'.format('language'),
-            Media_Type=MEDIA_TYPE,
-            Download_URL='languages.geojson',
-        ))
 
-        return
-        for lg, _, _ in languoids:
-            args.writer.objects['LanguageTable'].append(dict(ID=lg.id, Name=lg.name, Glottocode=lg.id))
+                shapes = [
+                    (pids[0], f, gc) for pids, f, gc in features
+                    if gc and is_(fi[pids[0]].year, period)]
 
+                lids = None
+                contribs = {c['ID']: c for c in writer.objects['ContributionTable']}
+                for ptype in ['language', 'family']:
+                    label = 'languages' if ptype == 'language' else 'families'
+                    p = writer.cldf_spec.dir / '{}.geojson'.format(label)
+                    ffs, languages = aggregate(
+                        shapes,
+                        args.glottolog.api,
+                        level=ptype,
+                        buffer=0.005,
+                        opacity=0.5)
+                    dump(
+                        feature_collection(
+                            ffs,
+                            title='Speaker areas for {} ({})'.format(label, period),
+                            description='Speaker areas aggregated for Glottolog {}-level languoids, '
+                            'color-coded by family.'.format(ptype)),
+                    p)
+                    for (glang, pids, family), f in zip(languages, ffs):
+                        if lids is None or (glang.id not in lids):  # Don't append isolates twice!
+                            writer.objects['LanguageTable'].append(dict(
+                                ID=glang.id,
+                                Name=glang.name,
+                                Glottocode=glang.id,
+                                Latitude=glang.latitude,
+                                Longitude=glang.longitude,
+                                Feature_IDs=pids,
+                                Maps=sorted(set(itertools.chain.from_iterable(contribs[pid]['Maps'] for pid in pids))),
+                                Speaker_Area=p.stem,
+                                Glottolog_Languoid_Level=ptype,
+                                Family=family,
+                            ))
+                    writer.objects['MediaTable'].append(dict(
+                        ID=p.stem,
+                        Name='Speaker areas for {} ({})'.format(label, period),
+                        Description='Speaker areas aggregated for Glottolog {}-level languoids, '
+                                    'color-coded by family.'.format(ptype),
+                        Media_Type=MEDIA_TYPE,
+                        Download_URL=p.name,
+                    ))
+                    lids = {gl.id for gl, _, _ in languages}
 
+            writer.cldf.properties['dc:spatial'] = \
+                ('westlimit={:.1f}; southlimit={:.1f}; eastlimit={:.1f}; northlimit={:.1f}'.format(
+                    *self.bounds))
 
+    def cmd_readme(self, args):
+        minlon, minlat, maxlon, maxlat = self.bounds
+        coords = [[
+            (minlon, minlat),
+            (minlon, maxlat),
+            (maxlon, maxlat),
+            (maxlon, minlat),
+            (minlon, minlat)
+        ]]
+        f = json.dumps(Feature.from_geometry(dict(type='Polygon', coordinates=coords)))
+        return add_markdown_text(
+            cldfbench.Dataset.cmd_readme(self, args),
+            """
 
+### Coverage
 
-        features, languoids = aggregate(list(self.iter_polys()), args.glottolog.api, level='family')
-        print(len(languoids))
-        dump(dict(type='FeatureCollection', features=features), self.cldf_dir / 'families.json')
-        #for lg, _, _ in languoids:
-        #    args.writer.objects['LanguageTable'].append(dict(ID=lg.id, Name=lg.name, Glottocode=lg.id))
+```geojson
+{}
+```
+""".format(f),
+            'Description')
